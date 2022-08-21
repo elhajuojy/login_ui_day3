7153c0da0d6d59d097c347b9236a8e235e78c465
elhajuojy <souirimehdi311@gmail.com>
2022-08-21 12:49:48 +0100

    welcome page

--- lib/screens/welcomepage.dart
+++ lib/screens/welcomepage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:login_ui_day3/core/constant/color.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -8,8 +10,26 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("WelcomePage"),
+      backgroundColor: appColors.backgroundColor,
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 450,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.orange,
+                    image: const DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/images/one.jpg'),
+                    )),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
